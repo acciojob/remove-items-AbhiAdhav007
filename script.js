d132cfa74a698123c211c49b8422a79767d1aa13
AbhiAdhav007 <120732315+AbhiAdhav007@users.noreply.github.com>
2023-05-27 20:56:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function deleteElements(){
+	let selectedElement = document.getElementById("colorSelect");
+	let selectedValue = selectedElement.value;
+	let childrenList = selectedElement.children;
+	let retainedElements = [];
+	 for(let i=0 ;i< childrenList.length ; i++)
+		 {
+			 if(selectedValue===childrenList[i].value)
+			 {
+				 // retainedElements.push(selectedValue)
+				 childrenList[i].remove();
+			 }
+				 
+		 }
+}
